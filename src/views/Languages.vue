d4b6173925ee3f5c89cc50e96521b470c1b640d3
mmfiber <masa.03191996@gmail.com>
2019-10-07 22:45:04 +0900

    add lang page

---
+++ src/views/Languages.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  BaseLayout
+</template>
+
+<style lang="stylus" scoped>
+
+</style>
+
+<script>
+import BaseLayout from '../components/BaseLayout';
+
+export default {
+  components: {
+    BaseLayout,
+  },
+};
+</script>
