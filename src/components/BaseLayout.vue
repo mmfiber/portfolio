c75ec5eb77f39c8fd045433c359ec2d5a1d6fc6b
mmfiber <masa.03191996@gmail.com>
2019-10-07 21:16:09 +0900

    basic home

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   .baselayout
-    HeaderBar.header.mb-5
+    HeaderBar#header.mb-5
     slot
   
 </template>
 
 <style lang="stylus" scoped>
-  .header
+  #header
     position sticky
     top 0
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,28 @@
-<template>
-  <HelloWorld />
+<template lang="pug">
+  v-container
+    v-row.justify-center.align-center.fill-height
+      .wrapper
+        span.display-3.mb-6 Welcome to Masahiro Mikami's Portfolio Site
+        br
+        span.display-1 Click to start
 </template>
 
-<script>
-import HelloWorld from '../components/HelloWorld';
+<style lang="stylus" scoped>
+  .container
+    height 100%
 
+  .wrapper>span
+    display inline-block
+    width 100%
+    text-align center
+</style>
+
+<script>
 export default {
-  components: {
-    HelloWorld,
-  },
+  computed:{
+    headerHeight(){
+      return document.getElementById('header').clientHeight + 20 // mb-5:20px
+    }
+  }
 };
 </script>
