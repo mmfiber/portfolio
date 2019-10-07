1dd52eb91fc186006d232ff00652ae4c1d352487
mmfiber <masa.03191996@gmail.com>
2019-10-07 15:29:34 +0900

    add about vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
   BaseLayout
+    v-container
+      v-row.justify-center.mb-5
+        v-flex.xs8.sm3
+          img.icon(src="../assets/icon.svg")
+      v-row.justify-center
+        v-flex.xs8.sm6 text
 </template>
+
+<style lang="stylus" scoped>
+  .v-row
+    width 100%
+
+  .icon
+    width 100%
+</style>
 
 <script>
 import BaseLayout from '../components/BaseLayout';
