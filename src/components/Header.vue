be756e6a3c5891080730292d1c23329dab1808b0
mmfiber <masa.03191996@gmail.com>
2019-10-07 13:52:57 +0900

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  v-toolbar
+    v-layout(align-center justify-center)
+      span.xs2.px-5.mx-5(v-for="val in contents") {{val}}
+</template>
+
+<style lang="stylus" scoped>
+  .v-toolbar 
+    max-height 64px
+
+    span
+      font-size 24px
+</style>
+  
+<script>
+export default {
+  data(){
+    return {
+      contents:[
+        "Home",
+        "About",
+        "Languages",
+        "Works",
+        "Contact"
+      ]
+    }
+  }
+};
+</script>
