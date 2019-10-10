8f1ede765eefa87838f74f2b5f8831786962b6e3
mmfiber <masa.03191996@gmail.com>
2019-10-10 22:32:06 +0900

    add wor page

---
+++ src/components/LinkBtn.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  v-btn(v-if="isInternal" :to="url"
+    :text="text" :color="color" x-large) {{btnText}}
+  a(v-else :href="url")
+    v-btn(:text="text" :color="color" x-large) {{btnText}}
+</template>
+
+<style lang="stylus" scoped>  
+  a
+    text-decoration none
+
+</style>
+
+<script>
+export default {
+  props:{
+    url: String,
+    btnText: String,
+    text: Boolean,
+    color: String,
+  },
+  data(){
+    return{
+      isInternal: this.isInternalLink()
+    }
+  },
+  methods:{
+    isInternalLink(){
+      return !this.url.match(/http(s)?:\/\//)
+    }
+  }
+}
+</script>
---
+++ src/components/WorkCard.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  v-card(elevation=8).mb-10
+    v-img(:alt="work.name" :src="require(`../assets/${work.eyecatch}`)") 
+    v-card-title.display-1.px-5 {{$t(`works.${work.name}.title`)}}
+    v-card-text.text.px-5 {{$t(`works.${work.name}.content`)}}
+    v-card-actions.btn.px-5
+      LinkBtn(:url="work.to" btnText="Go to page" :text="true" color="teal")
+      
+</template>
+
+<style lang="stylus" scoped>
+  .v-image
+    height 300px
+
+  .v-card
+    position relative
+
+    .text
+      font-size 20px
+      margin-bottom 68px
+        
+    .btn 
+      position absolute
+      right 0
+      bottom 0
+      
+  @media screen and (max-width: 1264px)
+    .v-image
+      height 200px
+    
+  
+</style>
+
+<script>
+import LinkBtn from "./LinkBtn"
+export default {
+  components:{
+    LinkBtn,
+  },
+  props:{
+    work: Object,
+  },
+}
+</script>
