6430b684d903ec5ce9650826ff76f16c13016ae6
mmfiber <masa.03191996@gmail.com>
2019-10-11 23:38:12 +0900

    LanguageBtn component

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -2,45 +2,42 @@
   .header
     v-toolbar(height="100%")
       v-toolbar-items
-        v-row.align-center.titles
+        v-app-bar-nav-icon(v-if="mobile" @click.stop="drawer = !drawer")
+        v-row(v-else).align-center.titles
           template(v-for="ctn in contents")
             router-link.fill-height(:to="getLink(ctn)")
               v-btn.px-10.display-1.capitalize(text
                 :style="{color: selectedCtn==ctn ? selectedColor : ''}") {{ctn}}
-        v-row.align-center.justify-end.lang
-          template(v-for="(lang, index) in langs")
-            span(v-if="index!=0").mx-2.display-1 /
-            v-btn.display-1.capitalize(@click="changeLang(lang)" text
-              :style="{color: selectedLang==lang ? selectedColor : ''}") {{lang}}
+        LanguageBtn.justify-end.lang(:selectedColor="selectedColor")
 </template>
 
 <style lang="stylus" scoped>
-  .v-container:after
-    display block
-    content ""
-    clear both
-
   .v-toolbar__items
     width 100%
 
     a
       text-decoration none
 
-  .titles
-    float left
+    .titles
+      float left
 
-  .lang
-    float right
-
+    .lang
+      float right
+    
+    .v-btn
+      height 100% !important
+  
   .capitalize
-    text-transform capitalize
-  
-  .v-btn
-    height 100% !important
-</style>
-  
+      text-transform capitalize
+  </style>
+    
 <script>
+import LanguageBtn from '../components/LanguageBtn'
+
 export default {
+  components:{
+    LanguageBtn,
+  },
   data(){
     return {
       contents:[
@@ -49,22 +46,19 @@ export default {
         "languages",
         "works",
       ],
-      langs: this.$i18n.availableLocales,
-      selectedColor: '#26A69A'
+      selectedColor: '#26A69A',
+      drawer: null,
     }
   },
   computed:{
     selectedCtn(){
       return this.$route.name
     },
-    selectedLang(){
-      return this.$i18n.locale
+    mobile(){
+      return parseInt(window.innerWidth) < 960
     },
   },
   methods:{
-    changeLang(lang){
-      this.$root.$i18n.locale = lang
-    },
     getLink(ctn){
       return ctn=='home' ? '/' : '/' + ctn
     }
---
+++ src/components/LanguageBtn.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  v-row.align-center
+    template(v-for="(lang, index) in langs")
+      span(v-if="index!=0").mx-2.display-1 /
+      v-btn.display-1.capitalize(@click="changeLang(lang)" text
+        :style="{color: selectedLang==lang ? selectedColor : ''}") {{lang}}
+</template>
+
+<style lang="stylus" scoped>  
+
+
+</style>
+
+<script>
+export default {
+  props:{
+    selectedColor: String,
+  },
+  data(){
+    return{
+      langs: this.$i18n.availableLocales,
+    }
+  },
+  computed:{
+    selectedLang(){
+      return this.$i18n.locale
+    },
+  },
+  methods:{
+   changeLang(lang){
+      this.$root.$i18n.locale = lang
+    },
+  }
+}
+</script>
